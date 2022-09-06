39bd1178e982eb4effc68c9b0a8bf1e88e1f3a53
feyzullah Demir <101548542+feyzullahdemir@users.noreply.github.com>
2022-09-06 15:00:08 +0300

    Update config.php

--- config.php
+++ config.php
@@ -9,11 +9,11 @@ class Config
     public static function options()
     {
         $options = new \Iyzipay\Options();
-        $options->setApiKey($this->settings['api_key'];);
+        $options->setApiKey($this->settings['api_key']);
         $options->setSecretKey($this->settings['secret_key']);
         $options->setBaseUrl($this->settings['api_type']);
         //Api key , secret ve BaseUrl buraya girmeniz gerekmektedir
-        //iyzico woocommerce aktif edince otomatik datadan çekecektir
+
 
         return $options;
     }
